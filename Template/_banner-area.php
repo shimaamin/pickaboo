c636621b2f18d4805a9cb8d3f4fdaea4b2e84730
shimaamin <69184399+shimaamin@users.noreply.github.com>
2021-05-01 21:39:27 +0600

    html
    html and css

---
+++ Template/_banner-area.php
@@ -0,0 +1,21 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="https://b2b-pickaboocdn.azureedge.net/media/emthemes/resized/frontend/emthemes/slideshow/p/-/p-protection-offer-v2-web.jpg" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="https://b2b-pickaboocdn.azureedge.net/media/emthemes/resized/frontend/emthemes/slideshow/c/i/city_bank_offer_web.jpg" alt="Banner1">
+        </div>
+        <div class="item">
+            <img src="https://b2b-pickaboocdn.azureedge.net/media/emthemes/resized/frontend/emthemes/slideshow/s/c/scb-offer-fb2.jpg" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="https://b2b-pickaboocdn.azureedge.net/media/emthemes/resized/frontend/emthemes/slideshow/a/i/air-conditioner-_big-banner.jpg" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="https://b2b-pickaboocdn.azureedge.net/media/emthemes/resized/frontend/emthemes/slideshow/x/i/xiaomi_tv_stick_web-post.jpg" alt="Banner2">
+        </div>
+    </div>
+</section>
+<!-- !Owl-carousel -->
---
+++ Template/_blogs.php
@@ -0,0 +1,39 @@
+
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20 heading-secondary">
+            <span>Latest</span> Blogs
+        </h4>
+
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Oppo A53s 5G unveiled with Dimensity 700, 5,000mAh battery</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">The Oppo A53s 5G was teased recently on Flipkart, and today the phone makes its official debut. It comes with a Dimensity 700 chipset, two RAM options, a big battery and three cameras on the back.</p>
+                    <a href="#" class="color-second text-left">Read More</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Realme announces Realme UI 2.0 early access program for X7 Pro</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Realme today announced the Realme UI 2.0 early access program for the X7 Pro in India. The Realme UI 2.0 is based on Android 11 it a try can apply for early access by heading to their X7 Pro's Settings > Software Update</p>
+                    <a href="#" class="color-second text-left">Read More</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">iOS 14.5 brings Dual SIM 5G support to iPhone 12 series</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Yesterday Apple released iOS 14.5 alongside iPadOS 14.5 and watchOS 7.4 to its users worldwide. One more under the radar feature with the new update for iPhone 12 series</p>
+                    <a href="#" class="color-second text-left">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
